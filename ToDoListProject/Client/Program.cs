d9fcdf862dc4ed045dea52973424ef537a8e66c8
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-08-11 10:43:34 +0300

    Add files via upload

--- ToDoListProject/Client/Program.cs
+++ ToDoListProject/Client/Program.cs
@@ -1,25 +1,37 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using TodoList.Client.Services;
+using TodoList.Client.Shared;
 
-namespace ToDoListProject.Client
+namespace TodoList.Client
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<AppStateContainer>();
 
-            await builder.Build().RunAsync();
+            builder.Services.AddScoped<ITodosService, TodosService>();
+            builder.Services.AddScoped<ITodoListsService, TodoListsService>();
+            builder.Services.AddScoped<TodosService>();
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddScoped<IUsersService, UsersService>();
+            builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
+            builder.Services.AddScoped<IHttpService, HttpService>();
+            var host = builder.Build();
+
+            var authenticationService = host.Services.GetRequiredService<IAuthenticationService>();
+            await authenticationService.Initialize();
+
+            await host.RunAsync();
         }
     }
 }
