0e3e274e8c1bedf48295f6daf5259e3492dafe2c
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-08-11 10:56:02 +0300

    Add files via upload

--- ToDoListProject/Server/Pages/Error.cshtml.cs
+++ ToDoListProject/Server/Pages/Error.cshtml.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ToDoListProject.Server.Pages
+namespace TodoList.Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
---
+++ ToDoListProject/Server/Repositories/ITodoListsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoList.Server.Models;
+
+namespace TodoList.Server.Repositories
+{
+    public interface ITodoListsRepository
+    {
+        Task<IEnumerable<ListOfTodos>> GetTodoListsAsync(int userId);
+        Task<ListOfTodos> GetTodoListAsync(int userId, int todoListId);
+        Task<bool> ListOfTodosExists(int userId, string title);
+        Task<bool> ListOfTodosExists(int userId, int listId);
+        void UpdateTodoList(ListOfTodos listOfTodos);
+    }
+}
